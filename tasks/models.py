18eb842aa797ea4e035a154689e3f7433d43b5c4
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-03-17 11:34:56 -0600

    Usuario personalizado con AbstractBaseUser

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self,email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Los usuarios deben tener un email')
+        
+        user = self.model(
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
+            # is_active = is_active,
+            # is_staff = is_staff,
+        )
+        
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name,password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            # is_active=is_active,
+            # is_staff=is_staff
+        )
+        
+        user.is_admin = True
+        user.save(using=self._db)
+        return user 
+    
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+    
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True 
+    
+    def has_module_perms(self, app_label):
+        return True 
+    
+    @property
+    def is_staff(self):
+        return self.is_admin     
