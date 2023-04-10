fe09a7644cbabdd36c3d66d72dae4fe517a120f6
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-04-10 11:46:58 -0600

    aAñadiendo ModelViewset

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Tasks
+from .serializers import UserSerializer, TasksSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Tasks.objects.all()
+    serializer_class = TasksSerializer
